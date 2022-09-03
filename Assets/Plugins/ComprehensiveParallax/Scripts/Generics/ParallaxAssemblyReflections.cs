a5c8e0e709c19112236a61b0a0d30812f8e4c146
Arcturus <arcturusforge@gmail.com>
2022-09-03 22:28:36 +1000

    Update ParallaxAssemblyReflections.cs
    + No longer using reflection.
    + Works regardless of preset/header script placement.

--- Assets/Plugins/ComprehensiveParallax/Scripts/Generics/ParallaxAssemblyReflections.cs
+++ Assets/Plugins/ComprehensiveParallax/Scripts/Generics/ParallaxAssemblyReflections.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 /// <summary>
 /// https://forum.unity.com/threads/finding-all-scripts-of-a-certain-type-not-monobehavior.918866/
@@ -16,17 +15,20 @@ namespace Arcturus.Parallax.Internal
         {
             var headerList = new List<PresetHeaderBase>();
 
-            // Compile a list of all existing Script Types that inherit from PresetHeaderBase.
-            var typeList = Assembly.GetAssembly(typeof(PresetHeaderBase)).GetTypes().Where(type => type.IsSubclassOf(typeof(PresetHeaderBase))).ToList();
-
-            // Interate through the list of Types.
-            foreach (var type in typeList)
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                // Create a temporary instance of these PresetHeader scripts.
-                var instance = (PresetHeaderBase)Activator.CreateInstance(type);
+                // Compile a list of all existing Script Types that inherit from PresetHeaderBase.
+                var typeList = assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(PresetHeaderBase))).ToList();
 
-                // Store temp instance in list.
-                headerList.Add(instance);
+                // Interate through the list of Types.
+                foreach (var type in typeList)
+                {
+                    // Create a temporary instance of these PresetHeader scripts.
+                    var instance = (PresetHeaderBase)Activator.CreateInstance(type);
+
+                    // Store temp instance in list.
+                    headerList.Add(instance);
+                }
             }
             return headerList;
         }
